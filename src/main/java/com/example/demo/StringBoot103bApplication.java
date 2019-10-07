f37adb24329f075382055b303609ebcf2400c5ba
prasantib <prasanti.babu@gmail.com>
2019-10-07 14:06:20 -0400

    String book exercise 3b

---
+++ src/main/java/com/example/demo/StringBoot103bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot103bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot103bApplication.class, args);
+    }
+
+}
